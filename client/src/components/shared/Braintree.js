fa9b5eac8c5886bf252903323d48b83567b9e0fd
JoeJorgensen <joebjorg@gmail.com>
2022-04-20 12:04:50 -0600

    brain tree fully set up with be and fe

--- client/src/components/shared/Braintree.js
+++ client/src/components/shared/Braintree.js
@@ -1,31 +1,68 @@
-import react from 'react'
-import BraintreeDropin from 'braintree-dropin-react';
+import react, { useEffect, useState } from "react";
+import BraintreeDropin from "braintree-dropin-react";
 // import BraintreeSubmitButton from './BraintreeSubmitButton';
-import braintree from 'braintree-web-drop-in';
-import BraintreeSubmitButton from './BraintreeSubmitButton';
-import Card from '../Card';
+import braintree from "braintree-web-drop-in";
+import BraintreeSubmitButton from "./BraintreeSubmitButton";
+import Card from "../Card";
+import axios from "axios";
+import { UNSAFE_NavigationContext } from "react-router-dom";
 
-const Braintree = (props)=>{
+const Braintree = (props) => {
+  const [loaded, setLoaded] = useState(false);
+  const [token, setToken] = useState("");
+  const [amount, setAmount] = useState(100);
 
-    const handlePaymentMethod = (payload)=>{
-    
+
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  const getToken = async () => {
+    try {
+      let res = await axios.get("api/braintree_token");
+      setToken(res.data);
+      setLoaded(true);
+    } catch (error) {
+      alert("error getting token");
+    }
+  };
+  const handlePaymentMethod = async(payload) => {
+    console.log('amount',amount)
+
+    console.log('payload',payload)
+    console.log('nonce:', payload.nonce)
+
+    try {
+         let res = await axios.post('/api/payment', {amount, ...payload})
+         console.log('transaction id:', res.data)
+    } catch (error) {
+        //needs great error handling here
+        console.log('error',error.response)
+        alert('error receiving payment')
     }
 
-    return(
-        <Card>
-        <div>
-            <h1>Payments</h1>
-            <BraintreeDropin
-            braintree={braintree}
-            authorizationToken={"sandbox_gp2rpvy2_t42vyjm6387qnwxr"}
-            handlePaymentMethod={handlePaymentMethod}
-            renderSubmitButton={BraintreeSubmitButton}
-          />
+  };
+  if (!loaded) {
+    return <p>spinner</p>;
 
+  }
 
-        </div>
-        </Card>
-    )
-}
+  return (
+    <Card>
+      <div>
+        <h1>Payments</h1>
+        <p>Amount</p>
+        <input value={amount} onChange={(e)=>setAmount(e.target.value)} />
 
-export default Braintree
+        <BraintreeDropin
+          braintree={braintree}
+          authorizationToken={token}
+          handlePaymentMethod={handlePaymentMethod}
+          renderSubmitButton={BraintreeSubmitButton}
+        />
+      </div>
+    </Card>
+  );
+};
+
+export default Braintree;
